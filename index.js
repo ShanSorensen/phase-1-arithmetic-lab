d1d73bbccbf78836c20eb952ff50ad7910f447e2
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-21 10:55:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() * 10) +1;
+random;
+
+let num3 = 3
+let num4 = 1
+let mod = num3+num4;
+mod;
+
+let max = Math.max(20);
+max;
